c8bc41d9eb48b5d7c2d2e2a4f773baff9c24effb
andreykanava <66001105+andreykanava@users.noreply.github.com>
2022-08-29 18:51:58 +0300

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,6 @@
-api = "MdJvAfNRMtj65PcSddzUQXYVPMYvf4OCPPbk2NIjXRtOUEEFuObU7ccjlqctmZlP"
-secret_api = "PGoIeb7hAVu3fXayI0XjEbINYM2sbBahISD3yOaz5aO5W9elfymwU53wznLcJMYq"
+api = ""
+secret_api = ""
 laverage = 5
 tokens = ['FTMUSDT','ETHUSDT','SOLUSDT','ADAUSDT','ETCUSDT']
-discord_token = "OTg0NzQ2Nzk0MTU4Mjg4OTI3.G5yZLS.kmihMQ2KNOCnQhtCYTkoxRXyz9pB0ZZyd_lFKs"
+discord_token = ""
 bot_version = "1.1 nsl+fix (test)"
