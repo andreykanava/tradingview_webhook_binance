04ee86a183f48082281e7fdcd2fbeec385374a87
andreykanava <66001105+andreykanava@users.noreply.github.com>
2022-10-21 12:34:59 +0300

    Update info.py

--- info.py
+++ info.py
@@ -1,5 +1,5 @@
-api = "Do07scoC7dO6MMA3KDmmac8gDc1LoBS7qy2C0ZclKvgoDGAMFERPnhvdrGWQP6WD"
-secret_api = "wziXsU2UJBGrvihtG93YvCkZNeBDXT11Wt3ru9ynUA4dXd98aXC9RF6MHFsIY4ji"
+api = ""
+secret_api = ""
 laverage = 5
 tokens = ['ETHUSDT']
 bot_version = "3.2 test"
